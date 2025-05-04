6a4f0accb227d4829bc5231b6786e16087e766de
nayeem-miah <nayeem5113a@gmail.com>
2025-05-04 17:54:57 +0600

    redux-logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // product reducers
-const { createStore, combineReducers } = require("redux");
+const { createStore , applyMiddleware} = require("redux");
+const { default: logger } = require("redux-logger");
 // products constance
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCTS = "ADD_PRODUCTS";
@@ -41,81 +42,10 @@ const productReducer = (state= initialProducts, action) => {
 }
 
 // store creating
-// const store = createStore(productReducer);
+const store = createStore(productReducer, applyMiddleware(logger));
 
-// store.subscribe(() => {
-//     console.log(store.getState());
-// });
-// store.dispatch(getProducts());
-// store.dispatch(addProducts("potato"))
-
-
-// ------------------------------------------------
-// cart reduces
-
-const GET_CART_ITEM = "GET_CART_ITEM";
-const ADD_CART_ITEM = "ADD_CART_ITEM"; 
-
-// initial cart
-const initialCarts = {
-    carts: ["umbrella", "pen"],
-    numberOfCarts : 2
-}
-
-// cart counter
-const getCarts = () => {
-    return {
-        type: GET_CART_ITEM
-    }
-};
-
-const addCarts = (cart) => {
-    return {
-        type: ADD_CART_ITEM,
-        payload: cart
-    }
-};
-
-// creating reducer
-const cartReducer = (state = initialCarts, action) => {
-    switch (action.type){
-        case GET_CART_ITEM:
-            return {
-                ...state
-            };
-        case ADD_CART_ITEM:
-            return {
-                carts: [...state.carts, action.payload],
-                numberOfCarts: state.numberOfCarts + 1
-            };
-
-        default:
-         return state
-    }
-}
-
-// store
-// const cartStore = createStore(cartReducer);
-// cartStore.subscribe(() => {
-//     console.log(cartStore.getState());
-// });
-
-// cartStore.dispatch(getCarts());
-// cartStore.dispatch(addCarts("mobile"));
-
-// combineReducers -----------------------------------------
-const rootReducer = combineReducers({
-    productR: productReducer,
-    cartR : cartReducer
-})
-
-const combineStore = createStore(rootReducer);
-combineStore.subscribe(() => {
-    console.log(combineStore.getState());
+store.subscribe(() => {
+    console.log(store.getState());
 });
+store.dispatch(getProducts())
 
-combineStore.dispatch(getCarts())
-combineStore.dispatch(addCarts("orange"));
-
-combineStore.dispatch(getCarts());
-combineStore.dispatch(addProducts("laptop"));
