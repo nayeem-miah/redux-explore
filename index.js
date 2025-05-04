a9d72b708e0fb19712181e44c5ffcea07361c306
nayeem-miah <nayeem5113a@gmail.com>
2025-05-04 08:35:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// state
+const initialCounterState = {
+    count: 0
+};
+const initialUsersState = {
+    users: [{ name: "Nayeem" }]
+};
+
+// action ---> type , payload
+
+{
+    type: "INCREMENT"
+}
+// INCREMENT COUNTER
+// DECREMENT COUNTER
