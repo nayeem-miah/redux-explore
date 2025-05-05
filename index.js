93d2a10acc4ffd120ac45824f9c9bc029ddb435e
nayeem-miah <nayeem5113a@gmail.com>
2025-05-05 22:05:25 +0600

    applyMiddleware

--- index.js
+++ index.js
@@ -1,51 +1,78 @@
-// product reducers
-const { createStore , applyMiddleware} = require("redux");
-const { default: logger } = require("redux-logger");
-// products constance
-const GET_PRODUCTS = "GET_PRODUCTS";
-const ADD_PRODUCTS = "ADD_PRODUCTS";
-// 1. init state
-const initialProducts = {
-    products: ["sugar", "salt"],
-    numberOfProduct: 2
+// async actions - api calling
+// api url -- jsonplaceholder
+// middleware
+// axios api
+
+const { applyMiddleware,createStore } = require("redux");
+const { thunk } = require("redux-thunk");
+
+// consts
+const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+const GET_TODOS_FAILED = "GET_TODOS_FAILED";
+// state
+const initialTodosState = {
+    todos: [],
+    isLoading: false,
+    error: null
 };
 
-// 2.product counter
-const getProducts = () => {
+// actions
+const getTodosRequest = () => {
     return {
-        type: GET_PRODUCTS
+        type: GET_TODOS_REQUEST
+    };
+};
+const getTodosSuccess = (todos) => {
+    return {
+        type: GET_TODOS_SUCCESS,
+        payload: todos
+    };
+};
+
+const getTodosFailed = (error) => {
+    return {
+        type: GET_TODOS_FAILED,
+        payload: error
     }
 };
 
-const addProducts = (product) => {
-    return {
-        type: ADD_PRODUCTS,
-        payload : product    
-    }
-}
-
-// creating reducer 
-const productReducer = (state= initialProducts, action) => {
+// reducer ready
+const todosReducer = (state = initialTodosState, action) => {
     switch (action.type) {
-        case GET_PRODUCTS:
+        case GET_TODOS_REQUEST:
             return {
-                ...state
+                ...state,
+                isLoading: true,
             };
-        case ADD_PRODUCTS:
+        case GET_TODOS_SUCCESS:
             return {
-                products: [...state.products, action.payload],
-                numberOfProduct: state.numberOfProduct + 1
+                ...state,
+                isLoading: false,
+                todos: action.payload
             };
+        case GET_TODOS_FAILED:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            };
+    
         default:
-            return state
+            return state;
     }
+};
+
+// async actions creations
+const fetchData = async() => {
+    
 }
 
-// store creating
-const store = createStore(productReducer, applyMiddleware(logger));
+// store
+const store = createStore(todosReducer, applyMiddleware(thunk))
 
 store.subscribe(() => {
     console.log(store.getState());
-});
-store.dispatch(getProducts())
+})
 
+store.dispatch(fetchData());
